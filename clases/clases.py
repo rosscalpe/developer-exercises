5ab0ae31d635678983c0ce8f03d7ffefaa89855a
rosscalpe <ross.gusteban@gmail.com>
2022-10-03 21:38:38 -0300

    creacion de carpeta y archivo clases respecto al ejercicio 3

---
+++ clases/clases.py
@@ -0,0 +1,37 @@
+										
+# Escribir una clase en python llamada círculo que contenga un radio, con un método que
+# devuelva el área y otro que devuelva el perímetro del círculo.
+# Si se instancia la clase con radio <= 0 mostrar una excepción indicando un error amigable al
+# usuario e impidiendo la instanciación.
+# Si printeamos el objeto creado debe mostrarse una representación amigable.
+# El objeto debe tener su atributo radio modificable, si se le intenta setear un valor <= 0
+# mostrar un error y no permitir modificación.
+# Permitir la multiplicación del circulo: Circulo * n debe devolver un nuevo objeto con el radio
+# multiplicado por n. No permitir la multiplicación por números <= 0
+
+from tkinter import N
+
+
+class Circulo:
+    radio = int (input("Ingrese el radio del circulo: "))
+    #método para definir el área del circulo
+    def area(self):
+        return ("el área del circulo es: ", 3.14 * self.radio ** 2)
+    #método para definir el perímetro del circulo
+    def perimetro(self):
+        return ("El perímetro del círculo es: ", 2 * 3.14 * self.radio)
+    #método para permitir la multiplicación del circulo por n
+    def __mult__(self, n):
+        n = int (input("Ingrese el número por el que desea multiplicar el radio: "))
+        if n <= 0:
+            raise Exception("No se puede multiplicar por un numero negativo o cero")
+        else:
+            return self.radio * n
+
+newCirculo = Circulo();
+if newCirculo.radio <= 0:
+    raise Exception("Para crear un circulo, el radio debe ser mayor a 0");
+else:
+    print(newCirculo.area());
+    print(newCirculo.perimetro());
+    print(newCirculo.__mult__(N));
